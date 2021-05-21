acc59262b342fac2640409a668f8522341cc8e12
Wendel Lopes <wendelopes@msn.com>
2021-05-21 10:32:24 -0300

    Setting host local in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.hosts << "api.plamor-gerencial.dev"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
